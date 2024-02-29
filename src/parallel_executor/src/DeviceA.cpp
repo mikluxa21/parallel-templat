f9c27cc4f1446c39a792e2fd7ba461782b4286d1
mikluxa21 <teterevkolya21@gmail.com>
2024-02-29 13:00:39 +0300

    Working version for device

--- src/parallel_executor/src/DeviceA.cpp
+++ src/parallel_executor/src/DeviceA.cpp
@@ -1,2 +1,12 @@
 #include <parallel_executor/DeviceA.h>
 
+std::string DeviceA::getName()
+{
+  return "A";
+}
+
+std::string DeviceA::getDataAsString()
+{
+  return "A>> Read data";
+}
+
--- src/parallel_executor/src/DeviceB.cpp
+++ src/parallel_executor/src/DeviceB.cpp
@@ -1,2 +1,12 @@
 #include <parallel_executor/DeviceB.h>
 
+std::string DeviceB::getName()
+{
+  return "B";
+}
+
+std::string DeviceB::getDataAsString()
+{
+  return "1 2 3";
+}
+
