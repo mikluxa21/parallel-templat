2def5cabcec42e908ba89229476f4775db09ce02
mikluxa21 <teterevkolya21@gmail.com>
2024-03-01 13:05:31 +0300

    Work version for Test_EventQueue.cpp

--- src/parallel_executor/src/EventQueue.cpp
+++ src/parallel_executor/src/EventQueue.cpp
@@ -1,2 +1,29 @@
 #include <parallel_executor/EventQueue.h>
 
+
+void EventQueue::push(const std::shared_ptr<const Event> & event)
+{
+  std::lock_guard<std::mutex> lck(this->mutex);
+  this->queue.push(event);
+  this->cv.notify_one();
+}
+std::shared_ptr<const Event> EventQueue::pop(const std::chrono::seconds & duration)
+{
+  std::unique_lock<std::mutex> lck(this->mutex);
+  this->cv.wait_for(lck, duration);
+  
+  if(this->queue.empty())
+  {
+    lck.unlock();
+    return nullptr;
+  }
+    
+  else{
+    std::shared_ptr<const Event> resEvent = this->queue.front();
+    this->queue.pop();
+    lck.unlock();
+    return resEvent;
+  }
+    
+}
+
