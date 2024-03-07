a594a3499a014098c2c9dc24922e8043b3a4246d
mikluxa21 <teterevkolya21@gmail.com>
2024-03-07 12:29:21 +0300

    Updated read for pushing Events, Updated run eith lambda functions->I think, what lam-func wilb be would delete

--- src/parallel_executor/src/Parser.cpp
+++ src/parallel_executor/src/Parser.cpp
@@ -1,18 +1,19 @@
 #include <parallel_executor/Parser.h>
 
-void Parser::run(size_t loop_count_A, size_t loop_count_B, int crush_index_A = -1, int crush_index_B = -1)
+void Parser::run(size_t loop_count_A, size_t loop_count_B, int crush_index_A, int crush_index_B)
 {
-  std::thread thrA(this->read, A, 1, loop_count_A, crush_index_A);
-  std::thread thrB(this->read, B, 5, loop_count_B, crush_index_B);
+  auto lambdaRead = [this](std::shared_ptr<Device> device, std::chrono::seconds sleep_durations, size_t loop_count, int crush_index){this->read(device, sleep_durations, loop_count, crush_index);};
+  std::thread thrA(lambdaRead, A, 1, loop_count_A, crush_index_A);
+  std::thread thrB(lambdaRead, B, 5, loop_count_B, crush_index_B);
   while(1)
   {
-    auto resQueuePop;
+    std::shared_ptr<const Event> resQueuePop;
     //wait message from devise for 6 seconds
     for(size_t i = 0; i < 6 ; i++)
     {
-      resQueuePop =  = this->queue.pop();
-      if(resQueuePop == nullptr)
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+      resQueuePop = this->queue->pop(std::chrono::seconds(1));
+      if(resQueuePop != nullptr)
+        break;
     }
     //if there is not message
      if(resQueuePop == nullptr)
@@ -31,10 +32,10 @@ void Parser::run(size_t loop_count_A, size_t loop_count_B, int crush_index_A = -
 
 void Parser::read(std::shared_ptr<Device> device, std::chrono::seconds sleep_duration, size_t loop_count, int crush_index)
 {
+  this->queue->push(std::make_shared<const Event>(StartedEvent(device)));
   for(size_t i = loop_count; i > crush_index; i--)
     {
-      this->queue->push(dynamic_cast<>device);
+      this->queue->push(std::make_shared<const Event>(DataEvent(device)));
     }
-    if (crush_index > 0)
-      while(1){}
+    this->queue->push(std::make_shared<const Event>(WorkDoneEvent(device)));
 }
